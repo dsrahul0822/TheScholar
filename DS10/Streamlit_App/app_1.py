61baefc1382b2e51f8bf5fd721035257fd072e51
Rahul Tiwari <support@rahultiwari.co.in>
2023-08-30 23:29:15 +0530

    Add files via upload

--- DS10/Streamlit_App/app_1.py
+++ DS10/Streamlit_App/app_1.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import pandas as pd
 from datetime import datetime
+import base64
 
 # Function to load data from uploaded file
 def load_data(uploaded_file):
@@ -14,21 +15,17 @@ def concatenate_columns(row, columns):
 def clean_data(data, columns_to_concatenate=None, categorize_customer=False, threshold=25):
     if columns_to_concatenate:
         data['Concatenated_Column'] = data.apply(lambda row: concatenate_columns(row, columns_to_concatenate), axis=1)
-        
     if categorize_customer:
         data['Customer Category'] = data['total_bill'].apply(lambda x: 'Gold' if x >= threshold else 'Silver')
-    
     return data
 
-# Main app
+# Function to generate a link to download a file
+def get_file_download_link(file_name, file_data):
+    b64 = base64.b64encode(file_data).decode()
+    href = f'<a href="data:file/{file_name.split(".")[-1]};base64,{b64}" download="{file_name}">Download {file_name}</a>'
+    return href
+
 st.title("Scholar Data Cleaner")
-
-# Check if state variables exist, if not, initialize them
-if 'cleaned_df' not in st.session_state:
-    st.session_state.cleaned_df = None
-
-if 'file_name' not in st.session_state:
-    st.session_state.file_name = None
 
 # Sidebar for user inputs
 uploaded_file = st.sidebar.file_uploader("Upload a CSV file", type="csv")
@@ -44,22 +41,26 @@ if uploaded_file:
     categorize_choice = st.sidebar.checkbox('Categorize customers based on the threshold')
     
     if st.sidebar.button('Clean Data'):
-        st.session_state.cleaned_df = clean_data(df, columns_to_concatenate, categorize_customer=categorize_choice, threshold=threshold)
-    
-    # Check if cleaned data is available
-    if st.session_state.cleaned_df is not None:
-        st.write(st.session_state.cleaned_df)  # Displaying the cleaned data in the main area
+        cleaned_df = clean_data(df, columns_to_concatenate, categorize_customer=categorize_choice, threshold=threshold)
+        st.write(cleaned_df)  # Displaying the cleaned data in the main area
         
         # Export functionality
         current_time = datetime.now().strftime("%Y%m%d_%H%M%S")
         export_format = st.sidebar.radio('Select file format to export', ['Excel', 'CSV'])
         if st.sidebar.button('Export Cleaned Data'):
             if export_format == 'Excel':
-                st.session_state.file_name = f'data_cleaned_{current_time}.xlsx'
-                st.session_state.cleaned_df.to_excel(st.session_state.file_name, index=False)
+                file_name = f'data_cleaned_{current_time}.xlsx'
+                with st.sidebar.beta_expander("Download Link", expanded=True):
+                    with open(file_name, 'wb') as f:
+                        cleaned_df.to_excel(f, index=False)
+                    with open(file_name, 'rb') as f:
+                        st.sidebar.markdown(get_file_download_link(file_name, f.read()), unsafe_allow_html=True)
             else:
-                st.session_state.file_name = f'data_cleaned_{current_time}.csv'
-                st.session_state.cleaned_df.to_csv(st.session_state.file_name, index=False)
-            st.sidebar.write(f"Data exported as {st.session_state.file_name}. Check your directory for the exported file.")
+                file_name = f'data_cleaned_{current_time}.csv'
+                with st.sidebar.beta_expander("Download Link", expanded=True):
+                    with open(file_name, 'wb') as f:
+                        cleaned_df.to_csv(f, index=False)
+                    with open(file_name, 'rb') as f:
+                        st.sidebar.markdown(get_file_download_link(file_name, f.read()), unsafe_allow_html=True)
 else:
     st.sidebar.warning("Please upload a CSV file.")
